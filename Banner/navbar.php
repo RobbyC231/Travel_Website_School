940d205a66aa8b20041fcb7ba6ce60b010421e42
brijesh golakiya <brijesh.golakiya21@gmail.com>
2018-05-30 14:42:33 -0600

    I created separate file for Navigation menu called navbar.php in Banner folder. You can call it in your header part by Banner/navbar.php

---
+++ Banner/navbar.php
@@ -0,0 +1,17 @@
+
+
+<div class="menu">
+  <div class="leftmenu">
+      <h4>PARADISE TRAVELS</h4>
+  </div>
+
+  <div class="rightmenu">
+        <ul>
+              <li id="firstlist"><a href="homepage.html"> HOME </a></li>
+              <li id="secondlist"><a href="TravPackages.html"> PACKAGE </a> </li>
+              <li id="thirdlist"><a href="customer_registration.php"> REGISTER </li>
+              <li id="fourthlist"><a href="contact_us.html"> CONTACT US </a></li>
+              <li id="fourthlist"><a href="customer_registration.php"> LOGIN </a></li>
+        </ul>
+  </div>
+ </div>
---
+++ homepage.php
@@ -0,0 +1,31 @@
+ <html>
+<head>
+      <title>Travel Agency</title>
+      <link href="homestyle.css" rel="stylesheet" type="text/css">
+      <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700%7CPT+Serif:400,700,400italic' rel='stylesheet'>
+
+      <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
+
+<body>
+          <div class="bgimage">
+                  <header>
+                    <?php
+                         include_once 'Banner/navbar.php';
+                    ?>
+                   </header>
+
+                   <main>
+                        <center><h1 id="offers_title"> Our Exciting Offers </h1> </center>
+                        <div class="package1">
+                                <h3> Bora Bora </h3>
+                        </div>
+                        <div class="package2">
+                                <h3> San Francisco </h3>
+                        </div>
+                        <div class="package3">
+                                <h3> New York </h3>
+                        </div>
+                   </main>
+          </div>
+</body>
+</html>
