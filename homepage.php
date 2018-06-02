e57759ea33f6827b9e20a4f3e66fcc84bcce8efe
brijesh golakiya <brijesh.golakiya21@gmail.com>
2018-06-02 11:16:03 -0600

    updates

--- homepage.php
+++ homepage.php
@@ -2,11 +2,29 @@
 <head>
       <title>Travel Agency</title>
       <link href="homestyle.css" rel="stylesheet" type="text/css">
-      <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700%7CPT+Serif:400,700,400italic' rel='stylesheet'>
+      <script type="text/javascript">
+                var cycle = 0;
+                var allBackgrounds = [ "images/background1.jpg",
+                     "images/background2.jpg",
+                     "images/background3.jpg",
+                     "images/background4.jpg",
+                     "images/background5.jpg" ];
 
-      <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
-
-<body>
+                function myFunction()
+                {
+                  if (cycle < 5)
+                  {
+                      document.body.style.backgroundImage = "url('" + allBackgrounds[cycle] + "')";
+                      cycle += 1;
+                  }
+                  else
+                  {
+                      cycle = 0;
+                  }
+                }
+      </script>
+</head>      
+<body onload="setTimeout(myFunction, 3000);">
           <div class="bgimage">
                   <header>
                     <?php
